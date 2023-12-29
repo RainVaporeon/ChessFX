64174a1af8353f1e9882719b58f678d78aa8dcad
Ori <38721999+OriSpirit@users.noreply.github.com>
2023-12-29 19:09:49 +0800

    make getImageView return a blank image instead of null

--- src/main/java/io/github/rainvaporeon/chessfx/utils/Images.java
+++ src/main/java/io/github/rainvaporeon/chessfx/utils/Images.java
@@ -9,7 +9,13 @@ public class Images {
 
     public static ImageView getImageView(String image) {
         InputStream stream = Images.class.getResourceAsStream(STR."/pieces/64/\{image}.png");
-        if(stream == null) return null;
+        if(stream == null) {
+            InputStream empty = Images.class.getResourceAsStream("/pieces/64/blank.png");
+            Image img = new Image(empty);
+            ImageView view = new ImageView(img);
+            view.resize(64, 64);
+            return view;
+        }
         Image img = new Image(stream);
         final ImageView imageView = new ImageView(img);
         imageView.resize(64, 64);
